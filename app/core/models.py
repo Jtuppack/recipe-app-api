ba6d829100a167a4e986894a148734ae16fdb389
Jtuppack <jtuppack@gmail.com>
2022-02-17 12:09:52 +1000

    Updated admin to support custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -19,7 +19,7 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, email, password):
         """Creates and saves a new super user"""
-        user = self.create_user(email, password)
+        user = self.create_user(email, password=password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -27,12 +27,13 @@ class ModelTests(TestCase):
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, "test123")
 
-    def test_create_new_superuser(self):
+    def test_new_superuser(self):
         """Test creating a new superuser"""
         user = get_user_model().objects.create_superuser(
-            'test@test.com'
+            'test@londonappdev.com',
             'test123'
         )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
